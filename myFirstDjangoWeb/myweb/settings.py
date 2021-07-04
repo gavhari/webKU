c9c5c6d8ce3f50d7f63e32aa9caadc4b972399c7
gavhari <gaberingo12@gmail.com>
2021-07-04 20:41:50 +0700

    commit 2

--- myFirstDjangoWeb/myweb/settings.py
+++ myFirstDjangoWeb/myweb/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webKU',
+        'USER': 'gabe',
+        'PASSWORD': 'semakinmanis12',
+        'HOST': '/var/run/mysqld/mysqld.sock'
     }
 }
 
